4e48ad33a361304c55fc3c6e6fa5d509d4415b1c
acdmAnderson <acdm.anderson@hotmail.com>
2024-11-09 23:53:21 -0300

    build: adding build and deploy files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import ExpressHttp from "./adapters/ExpressHttp"
 import { IHttp } from "./interfaces/IHttp"
+import * as doc from '../docs/swagger.json'
 
 const getHttp = (): IHttp => new ExpressHttp()
 
 const main = async (): Promise<void> => {
   const http = getHttp()
-  await http.listen(+(process.env.PORT ?? 9001))
+  await http.doc('/swagger', doc)
+  await http.listen(+(process.env.PORT ?? 9002))
   process.on('SIGINT', () => {
     console.log('Process is finishing')
     process.exit()
