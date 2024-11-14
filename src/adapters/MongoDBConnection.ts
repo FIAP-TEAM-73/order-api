70f9821e939549fbd7afca1d111116b5b5a69ac5
acdmAnderson <acdm.anderson@hotmail.com>
2024-11-13 21:34:02 -0300

    feat: creating MongoDBConnection

---
+++ src/adapters/MongoDBConnection.ts
@@ -0,0 +1,39 @@
+import IConnection from "../interfaces/IConnection";
+import { MongoClient } from "mongodb";
+
+const operations: string[] = ['insertOne', 'updateOne', 'deleteOne', 'find'];
+
+
+export default class MongoDBConnection implements IConnection {
+
+    private readonly client: MongoClient;
+
+    constructor(uri: string, private readonly database: string) {
+        this.client = new MongoClient(uri);
+    }
+
+    async connect(): Promise<void> {
+        await this.client.connect()
+        console.log('Successfully connected to database', this.database)
+    }
+
+    async getCollection(collection: string): Promise<any> {
+        return this.client.db(this.database).collection(collection)
+    }
+
+    async isAlive(): Promise<boolean> {
+        try {
+            await this.client.db(this.database).command({ ping: 1 });
+            return true;
+        } catch (error) {
+            console.log(`Error while validating DB connection ${this.database}. Error: ${JSON.stringify(error)}`)
+        }
+        return false;
+    }
+
+    async close(): Promise<void> {
+        console.log('Closing database connection', this.database)
+        await this.client.close()
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import ExpressHttp from "./adapters/ExpressHttp"
 import { IHttp } from "./interfaces/IHttp"
 import * as doc from '../docs/swagger.json'
+import MongoDBConnection from "./adapters/MongoDBConnection"
+
+const uri = `mongodb+srv://tech-challenge-order-api:${process.env.DB_PASSWORD ?? 1234}@cluster0.zwrft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const getHttp = (): IHttp => new ExpressHttp()
 
 const main = async (): Promise<void> => {
-  const http = getHttp()
-  await http.doc('/swagger', doc)
-  await http.listen(+(process.env.PORT ?? 9002))
-  process.on('SIGINT', () => {
-    console.log('Process is finishing')
-    process.exit()
-  })
+   const db = new MongoDBConnection(uri, 'tech-challenge-73')
+   await db.connect();
+   const http = getHttp()
+   await http.doc('/swagger', doc)
+   await http.listen(+(process.env.PORT ?? 9002))
+   process.on('SIGINT', async () => {
+      await db.close()
+      console.log('Process is finishing')
+      process.exit()
+   })
 }
 
 main().catch(console.log)
